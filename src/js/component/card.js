ab300da2ec46465542f06520fff65b9cb45d2854
Rodricasares <rodrigocantudo@gmail.com>
2022-08-29 10:54:01 +0000

    se crea componente slider para poder visualizar todos los personajes

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -2,39 +2,57 @@ import React, { useState, useContext, useEffect, useReducer } from "react";
 import "../../styles/home.css";
 import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 export const Card = () => {
   const { store, actions } = useContext(Context);
   const [init, setInit] = useState(0);
-  const [end, setEnd] = useState(21);
+  const [end, setEnd] = useState(52);
+
 
   useEffect(() => {
     actions.getCharacters();
   }, []);
 
-  console.log(store.characters);
-
   return (
     <div className="container ">
-      <div className="row row-cols-lg-12 ">
-        {store.characters.slice(init, end).map((item, index) => (
-          <div
-            className="col m-1 "
-            key={index}
-            onClick={() => actions.setID(item.id)}
-          >
-            <div className="card bg-dark" style={{ width: "9rem" }}>
-              <img
-                src={item.imageUrl}
-                className="smallCard"
-                alt="..."
-                style={{ width: "auto", height: "12rem" }}
-              />
-                <h6 className="card-title bg-dark text-light p-2">{item.fullName}</h6>
+      <motion.div className="slider-container ">
+
+        <motion.div
+          className="slider"
+          drag="x"
+          dragConstraints={{ right: 0, left: -13400 }}
+        >        
+        
+
+          {store.characters.slice(init, end).map((item, index) => (
+           
+           <div
+              className="col m-1 "
+              key={index}
+              onClick={() => actions.setID(item.id)}
+            > 
+              <motion.div className="item m-4 rounded-circle">   
+
+                <img
+                  src={item.imageUrl}
+                  className="smallCard"
+                  alt="..."
+                  style={{ width: "14rem", height: "14rem" }}
+                />
+                <h6
+                  className="card-title bg-dark text-light text-center p-2"
+                  style={{ width: "auto" }}
+                >
+                  {item.fullName}
+                </h6>
+              </motion.div>
             </div>
-          </div>
-        ))}
-      </div>
+            
+          ))}
+        </motion.div>
+      </motion.div>
+
     </div>
   );
 };
