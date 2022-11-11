9ee223aebaa92cd5b5813cce128c5b856b40d486
Rodricasares <rodrigocantudo@gmail.com>
2022-11-11 10:39:44 +0000

    package.json chages over script

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,7 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const Dotenv = require('dotenv-webpack');
-module.exports =  merge  (common, {
+module.exports = { merge } (common, {
     mode: 'production',
     output: {
         publicPath: './'
