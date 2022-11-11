02b40d315803ab5ee59d6b662162bc7445935a27
Rodricasares <rodrigocantudo@gmail.com>
2022-11-11 11:23:19 +0000

    reactivo image

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../styles/home.css";
 
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3 ">
 		
 			<div className="d-flex justify-content-center">
-		<img className="ms-5" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Game_of_Thrones_2011_logo.svg/2560px-Game_of_Thrones_2011_logo.svg.png" alt="Bootstrap" width="550" height="60"/>
+		<img className="m-2 gthrone" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Game_of_Thrones_2011_logo.svg/2560px-Game_of_Thrones_2011_logo.svg.png" alt="Bootstrap" width="550" height="60"/>
 			</div>
 		</nav>
 	);
